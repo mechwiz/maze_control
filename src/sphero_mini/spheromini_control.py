86dbdc635632e3cae25407d599996cbc07ed81b4
Mike Wiznitzer <mechanic.wiz@gmail.com>
2018-11-19 23:46:38 -0600

    added P trajectory controller to make sure the sphero bots don't get ahead of each other in their paths. still needs to be tested

--- src/sphero_mini/spheromini_control.py
+++ src/sphero_mini/spheromini_control.py
@@ -20,6 +20,13 @@ class sphero_control:
     def __init__(self):
         self.prey_time = time.time()
         self.predator_time = time.time()
+        self.prey_length = 0
+        self.predator_length = 0
+        self.prey_percent = 0
+        self.predator_percent = 0
+        self.prey_error = 0
+        self.predator_error = 0
+        self.kp = 1
         self.prey_limit = True
         self.predator_limit = True
         self.prey_achieved = []
@@ -64,9 +71,11 @@ class sphero_control:
             targetnum = len(self.prey_achieved)
             # print targetnum
             xt,yt = self.prey_pathpnt[targetnum]
+            self.prey_percent = self.get_precentTraj(targetnum,'prey',[xt,yt],[x,y])
 
             angle, distance = vector_to_target(x,yt,xt,y)
-            outspeed = self.prey_cntrl.getPIDSpeed(distance)
+            outspeed = self.prey_cntrl.getPIDSpeed(distance) - self.prey_error
+            outspeed = max(outspeed,0)
             # print angle, distance
 
             if distance < 20:
@@ -115,15 +124,24 @@ class sphero_control:
                 self.roll_sphero('Prey',outspeed,angle,self.prey_offset)
 
 
-        if len(self.prey_achieved)-len(self.predator_achieved)>20:
-            self.prey_limit = False
-            self.predator_limit = True
-        elif len(self.predator_achieved)-len(self.prey_achieved)>20:
-            self.prey_limit = True
-            self.predator_limit = False
-        else:
-            self.prey_limit = True
-            self.predator_limit = True
+        if self.prey_percent > 0 and self.predator_percent > 0:
+            error = self.prey_percent - self.predator_percent
+
+            if error > 0:
+                self.predator_error = 0
+                self.prey_error = self.kp*abs(error)
+            else:
+                self.prey_error = 0
+                self.predator_error = self.kp*abs(error)
+        # if len(self.prey_achieved)-len(self.predator_achieved)>20:
+        #     self.prey_limit = False
+        #     self.predator_limit = True
+        # elif len(self.predator_achieved)-len(self.prey_achieved)>20:
+        #     self.prey_limit = True
+        #     self.predator_limit = False
+        # else:
+        #     self.prey_limit = True
+        #     self.predator_limit = True
 
 
     # def predator_odom(self,data):
@@ -140,9 +158,11 @@ class sphero_control:
             targetnum = len(self.predator_achieved)
             # print targetnum
             xt,yt = self.predator_pathpnt[targetnum]
+            self.predator_percent = self.get_precentTraj(targetnum,'predator',[xt,yt],[x,y])
 
             angle, distance = vector_to_target(x,yt,xt,y)
-            outspeed = self.predator_cntrl.getPIDSpeed(distance)
+            outspeed = self.predator_cntrl.getPIDSpeed(distance) - self.predator_error
+            outspeed = max(outspeed,0)
             # print angle
 
             if distance < 20:
@@ -250,6 +270,39 @@ class sphero_control:
         self.predator_pathpnt = []
         for i in range(len(self.predator_path)):
             self.predator_pathpnt.append(self.waypnt_dict[self.predator_path[i]])
+
+        for i in range(1,len(self.prey_pathpnt)):
+            x1,y1 = self.prey_pathpnt[i-1]
+            x2,y2 = self.prey_pathpnt[i]
+            angle,distance = vector_to_target(x1,y1,x2,y2)
+            self.prey_length+=distance
+            x1,y1 = self.predator_pathpnt[i-1]
+            x2,y2 = self.predator_pathpnt[i]
+            angle,distance = vector_to_target(x1,y1,x2,y2)
+            self.predator_length+=distance
+
+    def get_precentTraj(self,num_achieved,animal,target,current):
+        length_tot = 0
+        xc,yc = current
+        xt,yt = target
+
+        if animal == 'prey':
+            pathpnt = self.prey_pathpnt
+            total = self.prey_length
+        else:
+            pathpnt = self.predator_pathpnt
+            total = self.predator_length
+
+        for i in range(num_achieved,len(self.prey_pathpnt)):
+            if i == num_achieved:
+                x1,y1 = xc,yc
+            else:
+                x1,y1 = pathpnt[i-1]
+            x2,y2 = pathpnt[i]
+            angle,distance = vector_to_target(x1,y1,x2,y2)
+            length_tot+=distance
+
+        return length_tot/total * 100.0
 
 def closest_node(node, nodes):
     nodes = np.asarray(nodes)
